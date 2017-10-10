0946b62a08e5e10080de960ed30c1755133a0662
Paul Jacks <paul.jacks@scopevisio.com>
2017-10-10 15:40:16 +0200

    error handler

--- src/HomeServerConfig.java
+++ src/HomeServerConfig.java
@@ -2,9 +2,16 @@ import handlers.QuestionHandler;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ErrorHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
 
 public class HomeServerConfig{
 
@@ -21,22 +28,19 @@ public class HomeServerConfig{
             s.start();
             s.join();
         } else {
-            throw new RuntimeException("Initialize the server before you start it");
+            throw new RuntimeException("Initialize the server before you start it, genious");
         }
     }
 
-    private HandlerList getAllServices(){
-
+    private HandlerList getAllServices() throws Exception{
         // File server & Context Handler for root, also setting the index.html
         // to be the "welcome file", i.e, autolink on root addresses.
-        ContextHandler resourceBase = new ContextHandler();
-        resourceBase.setContextPath(".");
-        ResourceHandler rHandler = new ResourceHandler();
-        rHandler.setDirectoriesListed(false);
-        rHandler.setWelcomeFiles(new String[]{ "index.html" });
-        rHandler.setResourceBase("../resource/new-websiter");
-        rHandler.setDirAllowed(true);
-        resourceBase.setHandler(rHandler);
+        ContextHandler rootContext = new ContextHandler();
+        rootContext.setContextPath(".");
+        rootContext.setHandler(getResourceHandlers());
+
+        // Get error handlers & set it!
+        rootContext.setErrorHandler(getErrorHandlers());
 
         // Possible servlet lists, for all servlets or custom services you want to access later.
         // Warning, it might become a little bit nested if you add to many classes.
@@ -46,10 +50,32 @@ public class HomeServerConfig{
         // Add the ResourceHandler to the server.
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] {
-                resourceBase ,
-                questionHandler});
+                rootContext ,
+                questionHandler,
+        });
 
         return handlers;
+    }
+
+    private ErrorHandler getErrorHandlers() throws Exception {
+        ErrorHandler e = new ErrorHandler();
+
+        // This be the error handler
+        ErrorPageErrorHandler errorHandler = new ErrorPageErrorHandler();
+        errorHandler.addErrorPage(404, "../resource/new-website/404.html");
+        e.addBean(errorHandler);
+        e.setServer(s);
+        return e;
+    }
+
+    private ResourceHandler getResourceHandlers(){
+        ResourceHandler rHandler = new ResourceHandler();
+        rHandler.setDirectoriesListed(false);
+        rHandler.setWelcomeFiles(new String[]{ "index.html" });
+        rHandler.setResourceBase("../resource/new-websiter");
+        rHandler.setDirAllowed(false);
+
+        return rHandler;
     }
 
 }
