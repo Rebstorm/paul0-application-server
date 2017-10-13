5089190b06378b4613cb4f74a61ec3dba8f3570d
Paul Jacks <paul.jacks@scopevisio.com>
2017-10-13 16:08:23 +0200

    absolute paths

--- paul0/paul0.sh
+++ paul0/paul0.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 SERVICE_NAME=paul0
 PATH_TO_JAR=bin/homeserver.jar
+JAR_ARGS="/home/paul/work/git/homeserver/paul0/resource/settings.xml"
 PID_PATH_NAME=/tmp/paul0-9078
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar $PATH_TO_JAR $JAR_ARGS /tmp 2>> /dev/null >> /dev/null &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
